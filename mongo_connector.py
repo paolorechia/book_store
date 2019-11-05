a7999c54b7df01247b7749c980df5def17207d31
Paolo Rechia <paolorechia@gmail.com>
2019-11-04 21:48:09 -0300

    Removidos campos 'id' das classes, utilizando o _id nativo do MongoDB; /PUT e /POST corrigidos

--- mongo_connector.py
+++ mongo_connector.py
@@ -1,3 +1,5 @@
+import sys
+
 port='27017'
 host='localhost'
 user='root'
@@ -8,13 +10,16 @@ database='book'
 #mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[database][?options]]
 from pymongo import MongoClient
 
+sys.stdout.write('Connecting to Mongo database...')
+sys.stdout.flush()
 client = MongoClient( 'mongodb://{}:{}@{}:{}'\
     .format(user, password, host, port)
 )
-
 book_db = client.book
 mongo_cursor  = book_db.test.find({})
 for c in mongo_cursor:
-    print(c)
     for k,v in c.items():
-        print(k, v)
+        continue
+
+sys.stdout.write(' succedeed!\n')
+sys.stdout.flush()
--- server.py
+++ server.py
@@ -4,23 +4,25 @@ from marshmallow import Schema, fields as mfields, pprint
 from marshmallow.exceptions import ValidationError
 from datetime import datetime
 from collections import namedtuple 
+from bson.objectid import ObjectId
 import os
 from mongo_connector import client, book_db
 
 app = Flask(__name__)
 api = Api(app)
 
-Location = namedtuple("Location", "id name")
+Location = namedtuple("Location", "name")
 locationModel = api.model('LocationModel', {
-    'id': fields.Integer,
+    '_id': fields.String,
     'name': fields.String
 })
 class LocationSchema(Schema):
-    id = mfields.Integer()
+    _id = mfields.Str()
     name = mfields.Str()
 
 Book = namedtuple("Book", "name author publication_date editor location leased")
 bookModel = api.model('BookModel', {
+    '_id': fields.Integer,
     'name': fields.String,
     'author': fields.String,
     'publication_date': fields.DateTime,
@@ -28,22 +30,33 @@ bookModel = api.model('BookModel', {
     'location': fields.String,
     'leased': fields.String
 })
+class BookSchema(Schema):
+    _id = mfields.Str()
+    name = mfields.String()
+    author = mfields.String()
+    publication_date = mfields.DateTime()
+    editor = mfields.String()
+    location = mfields.String()
+    leased = mfields.String()
 
-def mongo_cursor_to_list(mongo_cursor):
-    entities = [] 
-    for e in mongo_cursor:
-       del e['_id']
-       entities.append(e) 
-    return entities
+def validate_with_schema(schema, request):
+    schema_ = schema()
+    try:
+        e = schema.load(request.json)
+    except ValidationError as excp:
+        print(excp)
+        abort(400, excp)
+    return e
 
 @api.route('/locations')
 class LocationResource(Resource):
     @api.marshal_with(locationModel)
     def get(self):
-        locations = mongo_cursor_to_list(book_db.location.find({}))
+        locations = [ e for e in (book_db.location.find({})) ]
         return locations
 
     @api.expect(locationModel)
+    @api.marshal_with(locationModel)
     def put(self):
         schema = LocationSchema()
         try:
@@ -51,43 +64,55 @@ class LocationResource(Resource):
         except ValidationError as excp:
             print(excp)
             abort(400, excp)
-        fetched= book_db.location.find_one({'id': l['id']})
+        _id = l['_id']
+        del (l['_id'])
+        fetched = book_db.location.find_one({"_id": ObjectId(_id)})
+
         if fetched is None:
             abort(404, 'Not Found')
-        book_db.location.update_one({'id': l['id']}, {'$set': l})
+        book_db.location.update_one({'_id': ObjectId(_id)}, {'$set': l})
         return l
 
     @api.expect(locationModel)
+    @api.marshal_with(locationModel)
     def post(self):
-        schema = LocationSchema(exclude=["id"])
+        schema = LocationSchema(exclude=["_id"])
         try:
             l = schema.load(request.json)
         except ValidationError as excp:
             print(excp)
             abort(400, excp)
         book_db.location.insert_one(l)
+        print(l)
         return l
 
 @api.route('/locations/<id>')
 class LocationResource(Resource):
     @api.marshal_with(locationModel)
     def get(self, id):
-        id_ = int(id)
-        location = book_db.location.find_one({'id': id_ })
+        location = book_db.location\
+            .find_one({"_id": ObjectId(id)})
         if location is None:
             abort(404, 'Location not found')
         return location
-
     def delete(self, id):
         return {'location': 'delete'}
 
 @api.route('/books')
 class BookResource(Resource):
+    @api.marshal_with(bookModel)
     def get(self):
         return {'book': 'get'}
+    @api.expect(bookModel)
     def put(self):
         return {'book': 'put'}
+    @api.expect(bookModel)
     def post(self):
+        input_book = validate_with_schema(
+            BookSchema(exclude=["id"]),
+            request.json
+        )
+        book_db.book.insert_one({})
         return {'book': 'post'}
     def delete(self):
         return {'book': 'delete'}
