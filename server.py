044ee883de806f1ca9fb577e3b8b3b3862c67187
Paolo Rechia <paolorechia@gmail.com>
2019-11-23 15:33:59 -0300

    Experimentando VSCode com Python

--- server.py
+++ server.py
@@ -1,24 +1,25 @@
-from flask import Flask, escape, request, render_template 
-from flask_restplus import Resource, Api, fields, abort
-from flask_cors import CORS
+import json
+import os
+from collections import namedtuple
+from datetime import datetime
 
-from marshmallow import Schema, fields as mfields, pprint
+from bson import json_util
+from bson.objectid import ObjectId
+from flask import Flask, escape, render_template, request
+from flask_cors import CORS
+from flask_restplus import Api, Resource, abort, fields
+from marshmallow import Schema
+from marshmallow import fields as mfields
+from marshmallow import pprint
 from marshmallow.exceptions import ValidationError
 
-from datetime import datetime
-from collections import namedtuple 
-from bson.objectid import ObjectId
-from bson import json_util
+from mongo_connector import book_db, client
 
-import os
-import json
-
-from mongo_connector import client, book_db
-
-app = Flask(__name__) #, static_url_path='/static', static_folder='/static')
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app = Flask(__name__)  # , static_url_path='/static', static_folder='/static')
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 CORS(app)
 api = Api(app)
+
 
 @app.after_request
 def add_header(r):
@@ -29,28 +30,37 @@ def add_header(r):
     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     r.headers["Pragma"] = "no-cache"
     r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
+    r.headers["Cache-Control"] = "public, max-age=0"
     return r
 
+
 Location = namedtuple("Location", "name")
-locationModel = api.model('LocationModel', {
-    '_id': fields.String,
-    'name': fields.String
-})
+locationModel = api.model(
+    "LocationModel", {"_id": fields.String, "name": fields.String}
+)
+
+
 class LocationSchema(Schema):
     _id = mfields.Str()
     name = mfields.Str()
+    test = mfields.Str()
+
 
 Book = namedtuple("Book", "name author publication_date editor location leased")
-bookModel = api.model('BookModel', {
-    '_id': fields.String,
-    'name': fields.String,
-    'author': fields.String,
-    'publication_date': fields.DateTime,
-    'editor': fields.String,
-    'location': fields.String,
-    'leased': fields.String
-})
+bookModel = api.model(
+    "BookModel",
+    {
+        "_id": fields.String,
+        "name": fields.String,
+        "author": fields.String,
+        "publication_date": fields.DateTime,
+        "editor": fields.String,
+        "location": fields.String,
+        "leased": fields.String,
+    },
+)
+
+
 class BookSchema(Schema):
     _id = mfields.Str()
     name = mfields.String()
@@ -61,11 +71,11 @@ class BookSchema(Schema):
     leased = mfields.String()
 
 
-@api.route('/locations')
+@api.route("/locations")
 class LocationResource(Resource):
     @api.marshal_with(locationModel)
     def get(self):
-        locations = [ e for e in (book_db.location.find({})) ]
+        locations = [e for e in (book_db.location.find({}))]
         return locations
 
     @api.expect(locationModel)
@@ -78,14 +88,14 @@ class LocationResource(Resource):
             print(excp)
             abort(400, excp)
 
-        _id = l['_id']
-        del (l['_id'])
+        _id = l["_id"]
+        del l["_id"]
 
         fetched = book_db.location.find_one({"_id": ObjectId(_id)})
 
         if fetched is None:
-            abort(404, 'Not Found')
-        book_db.location.update_one({'_id': ObjectId(_id)}, {'$set': l})
+            abort(404, "Not Found")
+        book_db.location.update_one({"_id": ObjectId(_id)}, {"$set": l})
         return l
 
     @api.expect(locationModel)
@@ -100,33 +110,33 @@ class LocationResource(Resource):
         book_db.location.insert_one(l)
         return l
 
-@api.route('/locations/<id>')
+
+@api.route("/locations/<id>")
 class LocationResource(Resource):
     @api.marshal_with(locationModel)
     def get(self, id):
-        location = book_db.location\
-            .find_one({"_id": ObjectId(id)})
+        location = book_db.location.find_one({"_id": ObjectId(id)})
         if location is None:
-            abort(404, 'Location not found')
+            abort(404, "Location not found")
         return location
 
     def delete(self, id):
-        location = book_db.location\
-            .find_one({"_id": ObjectId(id)})
+        location = book_db.location.find_one({"_id": ObjectId(id)})
         if location is None:
-            abort(404, 'Location not found')
+            abort(404, "Location not found")
         book_db.location.delete_one({"_id": ObjectId(id)})
-        return {'Successfully deleted _id': id}
+        return {"Successfully deleted _id": id}
 
-@api.route('/books')
+
+@api.route("/books")
 class BookResource(Resource):
     @api.marshal_with(bookModel)
     def get(self):
-        books = [ e for e in (book_db.book.find({})) ]
-        return books 
+        books = [e for e in (book_db.book.find({}))]
+        return books
 
     @api.expect(bookModel)
-    @api.marshal_with(bookModel)   
+    @api.marshal_with(bookModel)
     def put(self):
         schema = BookSchema()
         try:
@@ -135,14 +145,14 @@ class BookResource(Resource):
             print(excp)
             abort(400, excp)
 
-        _id = b['_id']
-        del (b['_id'])
+        _id = b["_id"]
+        del b["_id"]
         fetched = book_db.book.find_one({"_id": ObjectId(_id)})
         if fetched is None:
-            abort(404, 'Not Found')
+            abort(404, "Not Found")
 
-        book_db.book.update_one({'_id': ObjectId(_id)}, {'$set': b})
-        b['_id'] = _id
+        book_db.book.update_one({"_id": ObjectId(_id)}, {"$set": b})
+        b["_id"] = _id
         return b
 
     @api.expect(bookModel)
@@ -155,29 +165,31 @@ class BookResource(Resource):
             print(excp)
             abort(400, excp)
         book_db.book.insert_one(input_book)
-        return input_book 
+        return input_book
 
-@api.route('/books/<id>')
+
+@api.route("/books/<id>")
 class BookIdResource(Resource):
     @api.marshal_with(bookModel)
     def get(self, id):
-        book = book_db.book\
-            .find_one({"_id": ObjectId(id)})
+        book = book_db.book.find_one({"_id": ObjectId(id)})
         if book is None:
-            abort(404, 'Location not found')
+            abort(404, "Location not found")
         return book
 
     def delete(self, id):
         book_db.book.delete_one({"_id": ObjectId(id)})
-        return {'Successfully deleted _id': id}
+        return {"Successfully deleted _id": id}
 
-@app.route('/index')
+
+@app.route("/index")
 def index_html():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    if os.getenv('FLASK_ENV') == 'development':
+    if os.getenv("FLASK_ENV") == "development":
         debug = True
     else:
         debug = False
-    app.run(host='0.0.0.0', port='4040', debug=debug)
+    app.run(host="0.0.0.0", port="4040", debug=debug)
