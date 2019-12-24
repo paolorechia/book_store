051780ac32f27142048278771cb29d3ec6beed82
Paolo Rechia <paolorechia@gmail.com>
2019-12-24 14:11:13 -0300

    Query argument.

--- server.py
+++ server.py
@@ -3,16 +3,17 @@ import os
 from collections import namedtuple
 from datetime import datetime
 
-from ariadne import QueryType, gql, make_executable_schema
+from ariadne import QueryType, ObjectType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
 from ariadne.constants import PLAYGROUND_HTML
 
 from mongo_connector import book_db, client
+from bson.objectid import ObjectId
 
 type_defs = gql("""
     type Query {
         hello: String!,
-        location: [Location!]!
+        location (_id: String, name: String): [Location!]!
     }
 
     type Location {
@@ -21,17 +22,21 @@ type_defs = gql("""
         test: String
     }
 """)
-
+location = ObjectType("Query")
 query = QueryType()
 
 @query.field("location")
-def resolve_location(_, info):
+def resolve_location(_, info, _id=None, name=None):
     request = info.context["request"]
     print(request)
     user_agent = request.headers.get("user-agent", "guest")
-    locations = book_db.location.find({})
+    if _id is not None:
+        locations = book_db.location.find({"_id": ObjectId(_id)})
+    elif name is not None:
+        locations = book_db.location.find({"name": name})
+    else:   
+        locations = book_db.location.find({})
     return locations
 
 schema = make_executable_schema(type_defs, query)
 app = GraphQL(schema, debug=True)
-
