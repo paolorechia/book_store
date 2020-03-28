63b17eb9ba1d0e081eedcb94c35992a49d38b8bc
Paolo Rechia <paolorechia@gmail.com>
2020-03-28 17:08:30 -0300

    Creates basic usage plan

--- backend/bin/use_access_token.sh
+++ backend/bin/use_access_token.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-api_url=https://2md4993fw9.execute-api.us-east-1.amazonaws.com/Dev
+api_url=https://2md4993fw9.execute-api.us-east-1.amazonaws.com/Dev/hello/
+access_token=
 
-curl -X GET -H "Authorization: $1" $api_url/hello
+curl -X GET -H "Authorization: $access_token" $api_url/hello
 
