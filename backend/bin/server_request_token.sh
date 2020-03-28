f2d5f6d2861f6413fb1ea25ac4deb285d93d18c7
Paolo Rechia <paolorechia@gmail.com>
2020-03-28 16:57:58 -0300

    Scripts

--- backend/bin/server_request_token.sh
+++ backend/bin/server_request_token.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-client_id=REPLACE
-client_secret=REPLACE
+client_id=$1
+client_secret=$2
 basic_auth=$(printf $client_id:$client_secret | base64 - )
-url=https://test-321.auth.us-east-1.amazoncognito.com/oauth2/token
+url="https://mytest-321.auth.us-east-1.amazoncognito.com/oauth2/token"
 curl -X POST -H "Authorization:Basic $basic_auth" $url -d "grant_type=client_credentials" --dump-header u && cat u
