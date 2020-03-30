12f01a84101efef867acde6b5045fb09cf213110
Paolo Rechia <paolorechia@gmail.com>
2020-03-30 09:09:48 -0300

    Swagger specification

---
+++ backend/swagger_example/handlers.py
@@ -0,0 +1,11 @@
+import json
+# import requests
+
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Request Validation Succedeed!",
+        }),
+    }
