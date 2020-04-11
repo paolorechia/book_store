a8a3021ff7498afe62730826da32c2f2061590e6
Paolo Rechia <paolorechia@gmail.com>
2020-04-10 21:35:29 -0300

    Fix style

--- bookstore/frontend/src/App.js
+++ bookstore/frontend/src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+import '@aws-amplify/ui/dist/style.css';
+
+
+Amplify.configure({
+  region: 'us-east-1',
+  userPoolId: 'us-east-1_W9ARdc2jp',
+  userPoolWebClientId: '6kr010lb4i9i3acduu3acpu28t',
+})
+
 
 function App() {
   return (
@@ -23,4 +35,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
